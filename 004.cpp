cc35717ff0327a066a1bd8338ebd18013b3d9311
bbw7561135 <32274080+bbw7561135@users.noreply.github.com>
2019-09-23 13:09:40 +0800

    Update 004.cpp

--- 004.cpp
+++ 004.cpp
@@ -27,3 +27,32 @@ int main()
 //Address updates = 0x6dfee8
 // updates = 0x6dfee8
 //Now updates = 7
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    double* p3 = new double [3];
+    p3[0] = 0.2;
+    p3[1] = 0.5;
+    p3[2] = 0.8;
+    //treat p3 like noraml array but p3 is atcually pointer
+    cout << "p3[1] is " << p3[1] << endl;
+
+    p3 = p3 + 1;//normal array name can not do it while pointer can
+    //here 1 refers to 8 byte as *p3 is double type //for int it is 4 byte
+    //now p3 pointers to p3[1] //
+    cout << "now p3[0] is " << p3[0] << endl; //0.5
+    cout << "now p3[1] is " << p3[1] << endl;
+    p3 = p3 - 1;
+    cout << "p3[0] is " << p3[0] << endl;
+    return 0;
+}
+
+//p3[1] is 0.5
+//now p3[0] is 0.5
+//now p3[1] is 0.8
+//p3[0] is 0.2
