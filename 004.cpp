8b131fb7e2401c89bf8e3fd9762131f93d07886a
bbw7561135 <32274080+bbw7561135@users.noreply.github.com>
2019-09-23 13:26:20 +0800

    Update 004.cpp

--- 004.cpp
+++ 004.cpp
@@ -56,3 +56,53 @@ int main()
 //now p3[0] is 0.5
 //now p3[1] is 0.8
 //p3[0] is 0.2
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    double wages[3] = {10000.0,20000.0,30000.0};
+    short  stacks[3] = {3,2,1};
+
+    //Here are two ways to get the address of an array
+    double* pw = wages; //name of an array = address
+    short* ps = &stacks[0]; //the first element's address is also the address of an array
+
+    cout << "pw = " << pw << ", *pw = " << *pw << endl;
+    pw = pw + 1;
+    cout << " add 1 to the pw point." << endl;
+    cout << "pw = " << pw << ", *pw = " << *pw << endl;
+
+    cout << "ps = " << ps << ", *pss = " << *ps << endl;
+    ps = ps + 1;
+    cout << " add 1 to the ps point." << endl;
+    cout << "ps = " << ps << ", *ps = " << *ps << endl;
+
+    cout << "access two elements via array notation." << endl;
+    cout << "stacks[0] = " << stacks[0]
+         << ", stacks[1] = " << stacks[1] << endl;
+    cout << "access two elements via pointer notation." << endl;
+    cout << "*stacks = " << *stacks
+         << ", *(stacks+1) = " << *(stacks+1) << endl;
+    //another difference between pointer and array name
+    cout << sizeof(wages) << " = size of wages array." << endl;
+    cout << sizeof(pw) << " = size of pw pointer." << endl;
+    return 0;
+}
+
+
+//pw = 0x6dfec0, *pw = 10000
+// add 1 to the pw point.
+//pw = 0x6dfec8, *pw = 20000
+//ps = 0x6dfeba, *pss = 3
+// add 1 to the ps point.
+//ps = 0x6dfebc, *ps = 2
+//access two elements via array notation.
+//stacks[0] = 3, stacks[1] = 2
+//access two elements via pointer notation.
+//*stacks = 3, *(stacks+1) = 2
+//24 = size of wages array.
+//4 = size of pw pointer.
