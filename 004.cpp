8a86cc945f3bf6037d95f24f5a817cfe8b1339be
bbw7561135 <32274080+bbw7561135@users.noreply.github.com>
2019-09-23 12:32:26 +0800

    Update 004.cpp

--- 004.cpp
+++ 004.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    int updates = 6;
+    int* p_updates;
+    p_updates = &updates;
+
+    //express values in two ways
+    cout << "Values: updates  = " << updates <<endl;
+    cout << " *updates  = " << *p_updates << endl;
+
+    //express address in two ways
+    cout << "Address updates = " << &updates <<endl;
+    cout << " updates = " << p_updates << endl;
+
+    //use pointer to change value
+    *p_updates = *p_updates + 1;
+    cout << "Now updates = " << updates << endl;
+    return 0;
+}
+
+//Values: updates  = 6
+// *updates  = 6
+//Address updates = 0x6dfee8
+// updates = 0x6dfee8
+//Now updates = 7
