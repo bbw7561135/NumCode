7d02fe726e9f79af733491ef568919154cb52962
bbw7561135 <32274080+bbw7561135@users.noreply.github.com>
2019-09-29 13:58:25 +0800

    Update 006.cpp

--- 006.cpp
+++ 006.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+
+int main()
+{
+    int i,m =10;
+    int days[m] = {1,2,3,4,5,6,7,8,9,10};
+
+    for(i=0;i<m;i++)
+    {
+        cout << "using days[i]" << days[i] << endl;
+    }
+
+    for(i=0;i<m;i++)
+    {
+        cout << "using *(days+i)" << *(days+i) << endl;
+    }
+
+    return 0;
+}
+
+
+
 
