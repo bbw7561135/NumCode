ec9482a208cc84dcc6a87ba574fdaf33a2fa801b
bbw7561135 <32274080+bbw7561135@users.noreply.github.com>
2019-10-09 02:02:52 +0800

    Update 009.cpp

--- 009.cpp
+++ 009.cpp
@@ -104,3 +104,52 @@ int main () {
             cout << "yes, &a is b"<<endl;
         return 0;
 }
+
+
+
+// vectors: overloading operators example
+#include <iostream>
+using namespace std;
+class CVector 
+{
+      public:
+        int x,y;
+        CVector () {};
+        CVector (int,int);
+        CVector operator + (CVector);
+        CVector operator = (CVector);
+};//delimeter
+    
+CVector::CVector (int a, int b)
+{
+        x = a;
+        y = b;
+}
+    
+CVector CVector::operator+ (CVector param)
+{
+        CVector temp;
+        temp.x = x + param.x;
+        temp.y = y + param.y;
+        return (temp);
+}
+
+CVector CVector::operator= (CVector param)
+{
+
+        x = param.x;
+        y = param.y;
+        return *this;
+}
+    
+int main () {
+        CVector a (3,1);
+        CVector b (1,2);
+        CVector c;
+        CVector d;
+        c = a + b;
+        d = a;
+        cout << c.x << "," << c.y << endl;
+        cout << d.x << "," << d.y << endl;
+        return 0;
+}
