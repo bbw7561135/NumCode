8b2d7ee438b9e98704a95dcf6896fb3ae193cc61
bbw7561135 <32274080+bbw7561135@users.noreply.github.com>
2019-10-09 01:57:41 +0800

    Update 009.cpp

--- 009.cpp
+++ 009.cpp
@@ -80,3 +80,27 @@ int main () {
         return 0;
 }
 
+
+// this
+#include <iostream>
+using namespace std;
+
+class CDummy
+{
+      public:
+        int isitme (CDummy& param);
+};
+
+int CDummy::isitme (CDummy& param)
+{
+        if (&param == this) return 1;
+        else return 0;
+}
+    
+int main () {
+        CDummy a;
+        CDummy* b = &a;
+        if ( b->isitme(a) )
+            cout << "yes, &a is b"<<endl;
+        return 0;
+}
