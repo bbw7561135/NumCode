cd8d27c0fd5695c0c7563b2b23a225e7e270d5f8
bbw7561135 <32274080+bbw7561135@users.noreply.github.com>
2019-10-15 23:25:25 +0800

    Update 011.cpp

--- 011.cpp
+++ 011.cpp
@@ -150,3 +150,46 @@ int main()
     return 0;
 }
 
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//题目：输入三个整数x,y,z，请把这三个数由小到大输出
+//程序分析：我们想办法把最小的数放到x上，先将x与y进行比较，如果x>y则将x与y的值进行交换， 
+//然后再用x与z进行比较，如果x>z则将x与z的值进行交换，这样能使x最小。
+
+
+int main()
+{
+    int x;
+    int y;
+    int z;
+    int temp;
+    cout << "input num1: " << endl;
+    cin >> x;
+    cout << "input num2: " << endl;
+    cin >> y;
+    cout << "input num3: " << endl;
+    cin >> z;
+    if(x>y)
+    {
+        temp = x;
+        x = y;
+        y = temp;
+    }
+    if(x>z)
+    {
+        temp = x;
+        x = z;
+        z = temp;
+    }
+    if(y>z)
+    {
+        temp = y;
+        y = z;
+        z = temp;
+    }
+    cout << x << '\t' << y << '\t' << z << endl;
+    return 0;
+}
