c2d342b4435abfbb6063c48afdff01c74724e58a
bbw7561135 <32274080+bbw7561135@users.noreply.github.com>
2019-10-15 23:04:18 +0800

    Update 011.cpp

--- 011.cpp
+++ 011.cpp
@@ -69,3 +69,25 @@ int main()
 
 }
 
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//题目：一个整数，它加上100后是一个完全平方数，再加上168又是一个完全平方数，请问该数是多少？ 
+//程序分析：在10万以内判断，先将该数加上100后再开方，再将该数加上268后再开方
+
+int main()
+{
+    long int i,x,y,z;
+    for(i=1;i<100000;i++)
+    {
+        x = sqrt(i+100);
+        y = sqrt(i+268);
+        if(x*x==i+100 && y*y==i+268)
+        {
+            cout << i << endl;
+        }
+    }
+    return 0;
+}
