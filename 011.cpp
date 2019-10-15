181d40e2cddf4144af7d8f3c94a54e800805adde
bbw7561135 <32274080+bbw7561135@users.noreply.github.com>
2019-10-15 23:15:24 +0800

    Update 011.cpp

--- 011.cpp
+++ 011.cpp
@@ -91,3 +91,62 @@ int main()
     }
     return 0;
 }
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//题目：输入某年某月某日，判断这一天是这一年的第几天？ 
+//程序分析：以3月5日为例，应该先把前两个月的加起来，然后再加上5天即本年的第几天
+//特殊情况，闰年且输入月份大于3时需考虑多加一天
+
+int main()
+{
+    int day;
+    int month;
+    int year;
+    int sum;
+    int leap;
+    cout << "input year: " << endl;
+    cin >> year;
+    cout << "input month: " << endl;
+    cin >> month;
+    cout << "input day: " << endl;
+    cin >> day;
+    switch(month)
+    {
+    case 1:sum=0;break;
+    case 2:sum=31;break;
+    case 3:sum=59;break;
+    case 4:sum=90;break;
+    case 5:sum=120;break;
+    case 6:sum=151;break;
+    case 7:sum=181;break;
+    case 8:sum=212;break;
+    case 9:sum=243;break;
+    case 10:sum=273;break;
+    case 11:sum=304;break;
+    case 12:sum=334;break;
+    default:cout<<"data error";break;
+    }
+    sum = sum + day;
+    if(year%400==0 || (year%4==0 && year%100!=0))
+    {
+        leap=1;
+    }
+    else
+    {
+        leap=0;
+    }
+
+    if(leap==1&&month>2)
+    {
+        sum = sum + 1;
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
+
