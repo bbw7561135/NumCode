5b4559252b72583d0f446a67ec57ea73d87b3ae7
bbw7561135 <32274080+bbw7561135@users.noreply.github.com>
2019-10-16 23:07:03 +0800

    Update 011.cpp

--- 011.cpp
+++ 011.cpp
@@ -447,3 +447,59 @@ int main()
 
     return 0;
 }
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//题目：一球从100米高度自由落下，每次落地后反跳回原高度的一半；再落下，求它在 
+//第10次落地时，共经过多少米？第10次反弹多高？
+
+
+
+
+int main()
+{
+    double sn = 100.0;
+    double hn = sn/2.0;
+    int n;
+
+    for(n=2;n<=10;n++)
+    {
+        sn = sn + 2*hn;
+        hn = hn/2;
+    }
+
+cout << "total path is " << sn << endl;
+cout << "10th back is " << hn << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
