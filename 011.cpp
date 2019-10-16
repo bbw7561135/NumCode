25eaa99682f0acdc859e33b8e12e1d211b6a2058
bbw7561135 <32274080+bbw7561135@users.noreply.github.com>
2019-10-16 23:17:15 +0800

    Update 011.cpp

--- 011.cpp
+++ 011.cpp
@@ -477,6 +477,64 @@ cout << "10th back is " << hn << endl;
 }
 
 
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//题目：猴子吃桃问题：猴子第一天摘下若干个桃子，当即吃了一半，还不瘾，又多吃了一个 
+//第二天早上又将剩下的桃子吃掉一半，又多吃了一个。以后每天早上都吃了前一天剩下 
+//的一半零一个。到第10天早上想再吃时，见只剩下一个桃子了。求第一天共摘了多少。
+
+
+
+
+
+int main()
+{
+    int day;
+    int x1;
+    int x2;
+
+    day=9;
+    x2=1;
+    while(day>0)
+    {
+        x1=(x2+1)*2;
+        x2=x1;
+        day--;
+    }
+
+    cout << x1 << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
